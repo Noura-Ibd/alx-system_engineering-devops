ec929fcef7a6a72b40e66a6e8bcbb47b4128677e
Noura-Ibd <nouraibadah@gmail.com>
2023-10-06 13:57:04 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - The main entry point of the program.
+ *
+ * This function serves as the entry point for the program.
+ * It initializes the kernel and performs the main processing tasks.
+ *
+ * Return: 0 on success, an error code on failure.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
