056e801fdfee5c80279b3b51e311b3122e982f70
Noura-Ibd <nouraibadah@gmail.com>
2023-10-06 12:48:36 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - The main entry point of the program.
+ *
+ * This function serves as the entry point for the program.
+ * It initializes the kernel and performs the main processing tasks.
+ *
+ * Return: 0 on success, an error code on failure.
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
