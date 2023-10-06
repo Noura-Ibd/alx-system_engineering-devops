85b9b908db26dcd6717628fe71b1a64e47d261eb
Noura-Ibd <nouraibadah@gmail.com>
2023-10-06 13:21:28 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - The main entry point of the program.
+ *
+ * This function serves as the entry point for the program.
+ * It initializes the kernel and performs the main processing tasks.
+ *
+ * Return: 0 on success, an error code on failure.
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 122; ch >= 97; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
