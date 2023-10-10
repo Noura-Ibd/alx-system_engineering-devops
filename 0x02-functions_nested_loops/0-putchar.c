5128a7d93bb16ab71f71e25d5c09a73342c53488
Noura-Ibd <nouraibadah@gmail.com>
2023-10-10 06:34:06 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - The main entry point of the program.
+ *
+ * This function serves as the entry point for the program.
+ * It initializes the kernel and performs the main processing tasks.
+ *
+ * Return: 0 on success, an error code on failure.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
